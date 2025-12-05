26e93fab64a001f52a3c906c391062437fa1f6d5
luismond <luismond.nlp@gmail.com>
2025-12-05 08:02:59 -0600

    update

--- wiki_api.py
+++ wiki_api.py
@@ -1,148 +1,72 @@
-"""
-wiki_api.py
+"""wiki api client."""
 
-A script to fetch and parse the HTML content of a Wikipedia page using the Wikimedia API.
-Retrieves the page's HTML, extracts all paragraphs, and collects their text.
-
-Environment Variables Required:
-- ACCESS_TOKEN: Your Wikimedia API access token.
-- APP_NAME: The name of your application (for User-Agent).
-- EMAIL: Contact email address (for User-Agent).
-
-
-Docs:
-
-Core REST API
-https://api.wikimedia.org/wiki/Core_REST_API
-
-
-Wikitext
-https://en.wikipedia.org/wiki/Help:Wikitext
-
-
-"""
-
-import os
-import requests
-from bs4 import BeautifulSoup as bs
-from dotenv import load_dotenv
-from time import sleep
+from config import MODEL
+from utils import *
 import pandas as pd
 from rich import print
-import warnings
-warnings.filterwarnings('ignore')
-
-load_dotenv()
-
-# ENV VARS
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-APP_NAME = os.getenv("APP_NAME")
-EMAIL = os.getenv("EMAIL")
-HEADERS = {'Authorization': f'Bearer {ACCESS_TOKEN}', 'User-Agent': f'{APP_NAME} ({EMAIL})'}
+from build_network_graph import draw_graph_pyvis
 
 
-# DATA
-with open('data/txt/exclude.txt', 'r') as fr:
-    exclude = [n.strip() for n in fr.readlines()]
+def crawl():
+    page_names = get_page_names()
+    shuffle(page_names)  
+    page_names_unrelated = get_page_names_unrelated()
 
-with open('data/txt/seed_names.txt', 'r') as fr:
-    seed_names = [n.strip() for n in fr.readlines()]
-
-
-# HTML FUNCS
-def get_html_soup(html_url):
-    response = requests.get(html_url, headers=HEADERS)
-    data = response.text
-    soup = bs(data, features="html.parser")
-    return soup
-
-
-def get_paragraphs_text(soup):
-    return [p.text for p in soup.find_all('p')]
-
-
-def get_paraphraphs_refs(soup):
-    """Extract all unique internal Wiki hrefs from <a> tags in <p> elements, skipping excluded names."""
-    hrefs = set()
-    for p in soup.find_all('p'):
-        for a in p.find_all('a'):
-            try:
-                href = a.get('href')
-                if href.startswith('.') and not any(e in href for e in exclude):
-                    hrefs.add(href[2:])
-            except AttributeError:
-                continue
-    return list(hrefs)
-
-
-def get_html_url(href):
-    return f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{href}/html'
-
-
-def get_short_desc(soup):
-    try:
-        shortdesc = soup.find('div', class_='shortdescription').text
-    except:
-        shortdesc = 'no_shortdesc'
-    return shortdesc
-
-
-def mine_rels():
-    """
-    Extract relationships between seed Wikipedia page names and other pages they link to in their main content
-    paragraphs. Produces a CSV file ('wiki_rels.csv') containing (source, target, relationship, target frequency).
-    """
-    rows = []
-    for page_name in seed_names[:5]:
-        sleep(.25)
-        print(page_name)
-        html_url = f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{page_name}/html'
-        soup = get_html_soup(html_url)
-        new_page_names = sorted(get_paraphraphs_refs(soup))
+    for page_name in page_names:
+        visited = set()
+        print(f'\n### reading {page_name} ###')
+        soup_ = get_soup(page_name)
+        new_page_names = get_paraphraphs_refs(soup_)
         for new_page_name in new_page_names:
+            exc = set(page_names + list(visited) + page_names_unrelated)
+            if new_page_name in exc:
+                continue
+            else:
+                soup = get_html_soup(new_page_name)
+                paragraphs = get_paragraphs_text(soup)
+                paragraphs_embedding = MODEL.encode_document(' '.join(paragraphs))
+                sim_score = float(MODEL.similarity(paragraphs_embedding, seed_corpus_embedding)[0])
+                if sim_score >= .5:
+                    save_new_page_name(new_page_name, soup, paragraphs, paragraphs_embedding)
+                else:
+                    with open('data/txt/page_names_unrelated.txt', 'a') as fa:
+                        fa.write(new_page_name+'\n')
+                visited.add(new_page_name)
+                
+        
+def mine_relationships():
+    page_names = get_page_names()
+
+    rows = []
+    for page_name in page_names:
+        soup_ = get_soup(page_name)
+        new_page_names = get_paraphraphs_refs(soup_)
+        for new_page_name in new_page_names:     
             rows.append((page_name, new_page_name))
+
+    if len(rows) == 0:
+        raise ValueError('zero rows')
 
     df = pd.DataFrame(rows)
     df.columns = ['source', 'target']
     df['relationship'] = 'co_occurs_with'
-    df['source_role'] = 'source_role'
-    df['target_role'] = 'target_role'
+
     df['target_freq'] = df['target'].map(df['target'].value_counts())
     df = df.drop_duplicates(subset=['source', 'target', 'relationship'])
+    df = df[df['source'].apply(lambda s: s not in exclude)]
+    df = df[df['target'].apply(lambda s: s not in exclude)]
     df.to_csv('data/csv/wiki_rels.csv', index=False, sep=',')
-
-
-def get_wiki_names_descriptions():
-    df = pd.read_csv('data/csv/wiki_rels.csv')
-    df = df[df['target_freq'] > 2]
-    tfd = dict(zip(df['target'], df['target_freq']))
-    names = set(df['source'].tolist() + df['target'].tolist()) 
-    print(len(names))
-
-    rows = []
-    for name in names:
-        sleep(.125)
-        print(name)
-        html_url = f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{name}/html'
-        soup = get_html_soup(html_url)
-        shortdesc = get_short_desc(soup)
-        rows.append((name, shortdesc))
-
-    df = pd.DataFrame(rows)
-    df.columns = ['name', 'shortdesc']
-    df['page_freq'] = df['name'].apply(lambda s: tfd.get(s, 0))
-    df.to_csv('data/csv/wiki_names_descs.csv', index=False, sep=',')
+    print(len(df))
+    print('relationships completed!')
 
 
 def main():
-    """
-    - start with 50 bookmarked wiki pages
-    - mine 150 more related pages
-    - build a dataset with page name, shortdesc, paragraphs, embeddings
-    - run queries
-    """
-    mine_rels()
-    #get_wiki_names_descriptions()
+    for _ in range(3):
+        crawl()
+        mine_relationships() #True False
+        draw_graph_pyvis(max_edges=1000)
+        #get_wiki_names_descriptions()
+        #get_wiki_names_paragraphs()
 
 if __name__ == "__main__":
     main()
