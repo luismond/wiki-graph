612277f3e6428c863d9e69f44b04b44ba2715392
luismond <luismond.nlp@gmail.com>
2025-12-07 03:34:18 -0600

    remove

--- config.py
+++ config.py
@@ -1,18 +1,1 @@
-import os
-from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
-import warnings
-warnings.filterwarnings('ignore')
-load_dotenv()
 
-# ENV VARS
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-APP_NAME = os.getenv("APP_NAME")
-EMAIL = os.getenv("EMAIL")
-HEADERS = {'Authorization': f'Bearer {ACCESS_TOKEN}', 'User-Agent': f'{APP_NAME} ({EMAIL})'}
-
-# MODELS
-MODEL = SentenceTransformer('distiluse-base-multilingual-cased-v1')
-
-# LANGUAGES
-LANG = 'en'
