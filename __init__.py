5f026531763539ddd442a2cf628c6b8339e340ef
luismond <luismond.nlp@gmail.com>
2025-12-11 01:48:18 -0600

    update paths

--- __init__.py
+++ __init__.py
@@ -2,3 +2,10 @@ import os
 
 BASE = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE, "data")
+CSV_PATH = os.path.join(DATA_PATH, "csv")
+TXT_PATH = os.path.join(DATA_PATH, "txt")
+EMBS_PATH = os.path.join(DATA_PATH, "embs")
+PARAGRAPHS_PATH = os.path.join(DATA_PATH, "paragraphs")
+SOUPS_PATH = os.path.join(DATA_PATH, "soups")
+
+page_relationships_file = os.path.join(CSV_PATH, 'page_relationships.csv')
