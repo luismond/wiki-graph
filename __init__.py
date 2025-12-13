a8a1bd0b1bd99a04568fa01e032912acda73bcdd
luismond <luismond.nlp@gmail.com>
2025-12-13 07:10:34 -0600

    Add logging functionality in __init__.py with a dedicated logger for improved tracking of application events.

--- __init__.py
+++ __init__.py
@@ -2,10 +2,22 @@ import os
 from dotenv import load_dotenv
 import warnings
 from datetime import datetime
+from logging import getLogger, FileHandler, Formatter, INFO
 
 warnings.filterwarnings('ignore')
 load_dotenv()
 
+def get_logger():
+    logger = getLogger('log.log')
+    logger.setLevel(INFO)
+    file_handler = FileHandler('log.log')
+    file_handler.setLevel(INFO)
+    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = get_logger()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 APP_NAME = os.getenv("APP_NAME")
@@ -17,9 +29,5 @@ HEADERS = {
 
 BASE = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE, "data")
-TXT_PATH = os.path.join(DATA_PATH, "txt")
-SOUPS_PATH = os.path.join(DATA_PATH, "soups")
 
 current_datetime_str = datetime.now().strftime('%Y-%m-%d-%H')
-
-pages_fp = os.path.join(DATA_PATH, 'pages.tsv')
