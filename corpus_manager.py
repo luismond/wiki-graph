ceef953939d8398b83eb8aa177876ca53e5f71a4
luismond <luismond.nlp@gmail.com>
2025-12-13 07:07:48 -0600

    Refactor CorpusManager to build and read paragraph corpus from SQLite database. Remove file-based corpus handling and enhance logging for corpus operations.

--- corpus_manager.py
+++ corpus_manager.py
@@ -1,14 +1,16 @@
 """
-Handles the collection of pages, tracks processed/unrelated pages,
-and provides access to the full dataset.
+Builds the paragraph corpus, embedding corpus, and saves to the database.
 """
 
 import os
 import pandas as pd
-from wiki_page import WikiPage
 import numpy as np
-from __init__ import SOUPS_PATH, DATA_PATH, current_datetime_str
+import sqlite3
 from sentence_transformers import SentenceTransformer
+
+from __init__ import DATA_PATH, current_datetime_str, logger
+from wiki_page import WikiPage
+
 MODEL = SentenceTransformer('distiluse-base-multilingual-cased-v1')
 
 
@@ -23,52 +25,64 @@ class CorpusManager:
     def __init__(self):
         self.corpus = None
         self.corpus_embedding = None
-        self.file_name = f"corpus_{current_datetime_str}.tsv"
         self.load()
-        self.load_corpus_embedding()
     
     def load(self) -> pd.DataFrame:
-        """
-        - If a corpus exists with the date name, read it
-        - If not, build the corpus and save it with the current date name
-        - This also ensures that the corpus embedding and the text corpus are aligned
-
-        """
-        if self.file_name in os.listdir(DATA_PATH):
-            self.corpus = self._read()
-        else:
-            self.corpus = self._build()
-            self._save()
+        self._build()
+        self.corpus = self._read()
 
     def _read(self)-> pd.DataFrame:
-        corpus = pd.read_csv(os.path.join(DATA_PATH, self.file_name), sep='\t')
-        unique_pages = len(corpus['page_name'].unique())
-        print(f'Read {self.file_name} ({corpus.shape} and {unique_pages} pages')
+        conn = sqlite3.connect('uap_ent.db')
+        cur = conn.cursor()
+        cur.execute("SELECT page_id, text, position FROM paragraph_corpus")
+        corpus = cur.fetchall()  # list of (page_id, paragraphs)
+        logger.info(f'Read paragraph_corpus with {len(corpus)} rows')
         return corpus
 
     def _build(self):
-        """Build a corpus from the saved pages' paragraphs."""
-        print(f'Building corpus...')
-        rows = []
-        for fn in os.listdir(SOUPS_PATH):
-            page_name = fn[:-4]
-            wp = WikiPage(page_name)
-            paragraphs = wp.paragraphs
-            for p in paragraphs:
-                if len(p.split()) > 5 and get_alpha_ratio(p) > .75:
-                    rows.append((page_name, p))
-        df = pd.DataFrame(rows)
-        df.columns = ['page_name', 'paragraphs']
+        """
+        Build the paragraph corpus from WikiPage objects 
+        and insert into the database if not present.
+        """
+        logger.info(f'Building corpus...')
+        conn = sqlite3.connect('uap_ent.db')
+        cur = conn.cursor()
+        cur.execute("SELECT id, name, sim_score FROM pages")
+        pages = cur.fetchall()
+
+        cur.execute("SELECT page_id FROM paragraph_corpus")
+        pc_page_ids = cur.fetchall()  # list of page_ids stored in paragraph_corpus table
+        pc_page_ids = set([p[0] for p in pc_page_ids])
+        logger.info(f'{len(pc_page_ids)} page_ids in paragraph_corpus table')
+
+        n = 0
+        for page_id, page_name, sim_score in pages:
+            if page_id not in pc_page_ids and sim_score >= .45:
+                wp = WikiPage(page_name)
+                paragraphs = [
+                    p for p in wp.paragraphs if len(p.split()) > 5 \
+                        and get_alpha_ratio(p) > .75]
+                for pos, pg in enumerate(paragraphs):
+                    cur.execute(
+                        "INSERT INTO paragraph_corpus (page_id, text, position) VALUES (?, ?, ?)",
+                        (page_id, pg, pos)
+                        )
+                    conn.commit()
+                    n += 1
+        logger.info(f'Added {n} pages to corpus')
+    
+    def to_df(self):
+        df = pd.DataFrame(self.corpus)
+        df.columns = ['page_id', 'text', 'position']
+        logger.info(f'Converted corpus to dataframe with shape {df.shape}')
         return df
 
-    def _save(self):
-        self.corpus.to_csv(os.path.join(DATA_PATH, self.file_name), index=False, sep='\t')
-        print(f'Saved corpus to {self.file_name} with shape {self.corpus.shape}')
-
     def load_corpus_embedding(self):
-        ce = CorpusEmbedding(self.corpus)
+        df = self.to_df()
+        ce = CorpusEmbedding(df)
         ce.load()
         self.corpus_embedding = ce.corpus_embedding
+         
 
 
 class CorpusEmbedding:
@@ -97,14 +111,14 @@ class CorpusEmbedding:
 
     def _read(self):
         corpus_embedding = np.load(self.file_path)
-        print(f'read {self.file_path} with shape {corpus_embedding.shape}')
+        logger.info(f'read {self.file_path} with shape {corpus_embedding.shape}')
         return corpus_embedding
 
     def _build(self):
-        print(f'encoding corpus...')
-        corpus_embedding = MODEL.encode_document(self.corpus['paragraphs'])
+        logger.info(f'encoding corpus...')
+        corpus_embedding = MODEL.encode_document(self.corpus['text'])
         return corpus_embedding
 
     def _save(self):
         np.save(self.file_path, self.corpus_embedding)
-        print(f'saved {self.file_name} with shape {self.corpus_embedding.shape}')
+        logger.info(f'saved {self.file_name} with shape {self.corpus_embedding.shape}')
