8238cb90fbcb910235c6c6b60801ec6e61ffee31
luismond <luismond.nlp@gmail.com>
2025-12-19 08:14:35 -0600

    Refactor WikiPage class: Enhance docstrings for clarity, improve method organization, and streamline database interactions for loading and saving page data.

--- wiki_page.py
+++ wiki_page.py
@@ -1,7 +1,10 @@
 """
-Utils to load, download and save a BeautifulSoup soup containing a wikipedia html page.
+WikiPage class.
 
-Also, utils to extract paragraph text and links from a soup.
+Represents a single Wikipedia page.
+
+Includes methods to download, parse, and extract links and paragraphs.
+Also, methods to save soups and page data to the database.
 """
 
 import requests
@@ -12,10 +15,6 @@ from __init__ import HEADERS, logger, DB_NAME, current_datetime_str
 
 
 class WikiPage:
-    """
-    Represents a single Wikipedia page, including methods to download,
-    parse, and extract links and paragraphs.
-    """
     def __init__(self, page_name: str, lang_code: str):
         self.page_name = page_name
         self.lang_code = lang_code
@@ -40,17 +39,22 @@ class WikiPage:
             f'https://api.wikimedia.org/core/v1/wikipedia/'
             f'{self.lang_code}/page/{self.page_name}/html'
         )
-        
+
     def get_soup(self) -> bs4.BeautifulSoup:
-        "Given a page name, either load the saved soup or download the soup."
+        """Either load the saved soup or download the soup."""
+
+        # Get the page table data, filtered by the page's lang code
         conn = sqlite3.connect(DB_NAME)
         cur = conn.cursor()
         cur.execute("""
         SELECT id, name, lang_code FROM pages
         WHERE lang_code = ?
-        """, (self.lang_code,))
+        """, (self.lang_code,)
+        )
         pages = cur.fetchall()
         page_id_dict = {name: id_ for id_, name, _ in pages}
+
+        # If the page name exists in the db, retrieve the associated soup
         if self.page_name in page_id_dict.keys():
             page_id = page_id_dict[self.page_name]
             conn = sqlite3.connect(DB_NAME)
@@ -60,14 +64,21 @@ class WikiPage:
             if row:
                 soup = pickle.loads(row[0])
             else:
-                # Fix this. It shouldn't be necessary to 're-download'
+                # Due to differing sim thresholds, it can happen that a page id exists
+                # without a corresponding saved soup.
+                # If a global sim value doesn't fix it, investigate the root cause.
+                # Alt., join the pages and soups table
+                # Alt., write an assertion line.
                 soup = self.download_soup()
         else:
             soup = self.download_soup()
         return soup
 
     def download_soup(self) -> bs4.BeautifulSoup:
-        "Given a page name, request a wikipedia url and return the parsed html page as a bs4 soup."
+        """"
+        Request a Wikipedia url
+        and return the parsed html page as a bs4 soup.
+        """
         response = requests.get(self.url, headers=HEADERS, timeout=180)
         soup = bs4.BeautifulSoup(response.text, features="html.parser")
         return soup
@@ -77,21 +88,25 @@ class WikiPage:
         assert self.page_id is not None
         conn = sqlite3.connect(DB_NAME)
         cur = conn.cursor()
-        cur.execute("INSERT OR REPLACE INTO soups (page_id, soup_data) VALUES (?, ?)", 
+        cur.execute("INSERT OR REPLACE INTO soups (page_id, soup_data) VALUES (?, ?)",
             (self.page_id, sqlite3.Binary(pickle.dumps(self.soup))))
         conn.commit()
 
     def save_page_name(self, sim_score):
+        """Save the page metadata in the pages table."""
+        # todo: rename method to "save_page_metadata"
         conn = sqlite3.connect(DB_NAME)
         cur = conn.cursor()
         cur.execute(
-        "INSERT OR IGNORE INTO pages (name, lang_code, url, crawled_at, sim_score) VALUES (?, ?, ?, ?, ?)",
+        "INSERT OR IGNORE INTO pages "
+        "(name, lang_code, url, crawled_at, sim_score) VALUES (?, ?, ?, ?, ?)",
         (self.page_name, self.lang_code, self.url, current_datetime_str, sim_score)
         )
         conn.commit()
         self.page_id = cur.lastrowid
 
     def get_shortdescription(self) -> str:
+        # todo: decide if remove or save it along the metadata
         try:
             shortdescription = self.soup.find('div', class_='shortdescription').text
         except:
@@ -100,6 +115,13 @@ class WikiPage:
 
     def get_paragraphs_text(self) -> list:
         "Return the text of all paragraphs."
+
+        def get_alpha_ratio(string: str) -> float:
+            """Calculate the ratio of alphabetic characters in a string."""
+            alpha_n = [ch for ch in string if ch.isalpha()]
+            alpha_ratio = len(alpha_n) / len(string)
+            return alpha_ratio
+
         paragraphs = []
         try:
             for p in self.soup.find_all('p'):
@@ -130,13 +152,14 @@ class WikiPage:
 
     def get_languages(self) -> list:
         """
-        Get a list of dictionaries containing the code, name, key, and title of the page languages.
+        Get a list of dictionaries containing the code, name,
+        key, and title of the page languages.
 
         Returns:
             A list of dictionaries. For example:
             [{'code': 'sv', 'name': 'svenska', 'key': 'Flygande_tefat', 'title': 'Flygande tefat'},
              {'code': 'th', 'name': 'ไทย', 'key': 'จานบิน', 'title': 'จานบิน'}]
-        
+
         Refer to: https://api.wikimedia.org/wiki/Core_REST_API/Reference/Pages/Get_languages
         """
         url = (
@@ -146,12 +169,3 @@ class WikiPage:
         response = requests.get(url, headers=HEADERS, timeout=180)
         languages = response.json()
         return languages
-
-
-
-def get_alpha_ratio(string: str) -> float:
-    """Calculate the ratio of alphabetic characters in a string."""
-    alpha_n = [ch for ch in string if ch.isalpha()]
-    alpha_ratio = len(alpha_n) / len(string)
-    return alpha_ratio 
-
