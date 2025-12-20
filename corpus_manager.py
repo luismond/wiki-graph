9e94bdd4fa39b307980f957a6984086a3ecd0b1c
luismond <luismond.nlp@gmail.com>
2025-12-20 02:02:11 -0600

    Refactor CorpusManager: Improve code readability by enhancing docstrings, restructuring methods, and optimizing database interactions. Update similarity search methods and add clustering functionality.

--- corpus_manager.py
+++ corpus_manager.py
@@ -2,9 +2,9 @@
 Builds the paragraph corpus, embedding corpus, and saves to the database.
 """
 
+import sqlite3
 import pandas as pd
 import numpy as np
-import sqlite3
 import torch
 from sentence_transformers.util import community_detection
 from __init__ import DB_NAME, MODEL, logger
@@ -13,15 +13,18 @@ from wiki_page import WikiPage
 
 class CorpusManager:
     """
-    The CorpusManager handles building and managing the Wikipedia paragraph corpus
-    from the database. It can load paragraphs and metadata, convert to a dataframe suitable
-    for downstream processing, and keep track of corpus and embedding arrays.
+    The CorpusManager builds and manages the Wikipedia paragraph corpus
+    from the database. It can load paragraphs and metadata,
+    convert to a dataframe suitable for downstream processing,
+    and keep track of corpus and embedding arrays.
 
     Key responsibilities:
-        - Build the paragraph-level corpus from existing pages and paragraphs in the DB.
-        - Load and access the corpus as structured data (e.g., Pandas DataFrame).
+        - Build the paragraph-level corpus from existing pages and paragraphs
+          in the DB.
+        - Load and access the corpus as structured data.
         - Prepare/coordinate corpus embeddings.
         - Provide convenient access to the main corpus and its properties.
+        - Provide access to vector similarity and clustering functions.
 
     - sim_threshold (float): Minimum similarity score for included pages.
     - corpus: List of (page_id, page_name, text, position) tuples.
@@ -29,10 +32,9 @@ class CorpusManager:
     - df: DataFrame view of the corpus.
 
     Usage:
-        cm = CorpusManager(sim_threshold=0.4)
-        cm.load()
+        cm = CorpusManager()
         df = cm.df
-        embeddings = cm.corpus_embedding
+        corpus_embedding = cm.corpus_embedding
     """
     def __init__(self, sim_threshold: float = .45):
         self.sim_threshold = sim_threshold
@@ -41,7 +43,8 @@ class CorpusManager:
         self.df = None
         self.load()
 
-    def load(self) -> pd.DataFrame:
+    def load(self):
+        """Initialize the module, build the corpus and load the vectors."""
         self._build()
         self.corpus = self._read()
         self.df = self._to_df()
@@ -52,7 +55,8 @@ class CorpusManager:
         conn = sqlite3.connect(DB_NAME)
         cur = conn.cursor()
         cur.execute("""
-            SELECT paragraph_corpus.id, page_id, pages.name, text, position, pages.lang_code
+            SELECT paragraph_corpus.id, page_id, pages.name, 
+            text, position, pages.lang_code
             FROM paragraph_corpus
             LEFT JOIN pages ON paragraph_corpus.page_id = pages.id
         """)
@@ -62,19 +66,22 @@ class CorpusManager:
 
     def _load_corpus_embedding(self):
         """
-        Load all paragraph embeddings from the database and stack them into a numpy array.
+        Load all paragraph embeddings from the database
+        and stack them into a numpy array.
 
-        This method fetches the binary embeddings from the `embedding` column in the
-        `paragraph_corpus` table, converts each to a float32 numpy array, and stacks
-        them vertically to produce a 2D array representing the corpus.
+        This method fetches the binary embeddings from the `embedding` column
+        in the `paragraph_corpus` table, converts each to a float32 np array,
+        and stacks them vertically to produce a 2D array.
 
         Sets:
-            self.corpus_embedding (np.ndarray): An array of shape (num_paragraphs, embedding_dim).
+            self.corpus_embedding (np.ndarray): 
+                An array of shape (num_paragraphs, embedding_dim).
         """
         conn = sqlite3.connect(DB_NAME)
         cur = conn.cursor()
         cur.execute("""SELECT embedding FROM paragraph_corpus""")
-        embeddings = [np.frombuffer(e[0], dtype=np.float32) for e in cur.fetchall()]
+        embeddings = [np.frombuffer(e[0], dtype=np.float32) \
+                      for e in cur.fetchall()]
         corpus_embedding = np.vstack(embeddings)
         self.corpus_embedding = corpus_embedding
         logger.info(f'Loaded embeddings with shape {corpus_embedding.shape}')
@@ -87,9 +94,13 @@ class CorpusManager:
         WHERE sim_score >= ?
         """, (self.sim_threshold,))
         pages = cur.fetchall()
-        logger.info(f'{len(pages)} page_ids in pages table with sim_score > {self.sim_threshold}')
+        logger.info(
+            f'{len(pages)} page_ids in pages table '
+            'with sim_score > {self.sim_threshold}'
+            )
         if len(pages) == 0:
-            raise ValueError(f'No pages found with sim_score > {self.sim_threshold}')
+            raise ValueError(f'No pages found with sim_threshold'
+                             f' > {self.sim_threshold}')
         conn.close()
         return pages
 
@@ -98,38 +109,40 @@ class CorpusManager:
         cur = conn.cursor()
         cur.execute("""SELECT page_id FROM paragraph_corpus""")
         pc_page_ids = cur.fetchall()
-        pc_page_ids = set([p[0] for p in pc_page_ids])
+        pc_page_ids = set(p[0] for p in pc_page_ids)
         logger.info(f'{len(pc_page_ids)} page_ids in paragraph_corpus table')
         return pc_page_ids
 
     def _build(self):
         """
-        From the pages table, get the pages with sim_score >= self.sim_threshold
+        Get the pages with sim_threshold >= self.sim_threshold
         and not in the paragraph_corpus table.
-        For each page, create a WikiPage object and save the paragraphs to the database.
+        For each page, create a WikiPage object and save it to the database.
         """
         logger.info('Building corpus...')
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
         pages = self._get_pages_table()
         pc_page_ids = self._get_corpus_page_ids()
-        # iterate over page ids and save paragraphs
+
         n = 0
         for page_id, page_name, lang_code, _ in pages:
-            if page_id not in pc_page_ids:
-                wp = WikiPage(page_name, lang_code=lang_code)
-                paragraphs = wp.paragraphs
-                if len(paragraphs) > 0:
-                    conn = sqlite3.connect(DB_NAME)
-                    cur = conn.cursor()
-                    for position, paragraph in enumerate(paragraphs):
-                        embedding = MODEL.encode(paragraph)
-                        embedding = np.array(embedding, dtype=np.float32).tobytes()
-                        cur.execute(
-                            "INSERT OR IGNORE INTO paragraph_corpus "
-                            "(page_id, text, embedding, position) VALUES (?, ?, ?, ?)",
-                            (page_id, paragraph, embedding, position)
-                        )
-                        conn.commit()
-                    n += 1
+            if page_id in pc_page_ids:
+                continue
+            wp = WikiPage(page_name, lang_code=lang_code)
+            paragraphs = wp.paragraphs
+            if len(paragraphs) == 0:
+                continue
+            for position, paragraph in enumerate(paragraphs):
+                embedding = MODEL.encode(paragraph)
+                embedding = np.array(embedding, dtype=np.float32).tobytes()
+                cur.execute(
+                    "INSERT OR IGNORE INTO paragraph_corpus "
+                    "(page_id, text, embedding, position) VALUES (?, ?, ?, ?)",
+                    (page_id, paragraph, embedding, position)
+                )
+                conn.commit()
+            n += 1
         logger.info(f'Added {n} pages to corpus')
 
     def _to_df(self):
@@ -140,17 +153,16 @@ class CorpusManager:
         logger.info(f'Converted corpus to dataframe with shape {df.shape}')
         return df
 
-    def similarity_search(self, query: str, top_k_min: int=100) -> pd.DataFrame:
-        """
-        Given a query, retrieve corpus rows that resemble the query.
-        Return the results in a dataframe, sorted by descending similarity.
-        """
+    def _similarity_search(self, query: str, top_k_min: int=100) -> list:
+        """Given a query, retrieve similar corpus rows."""
+        # todo: add lang code parameter
         # embeddings
         corpus_embeddings = self.corpus_embedding
         query_embedding = MODEL.encode_query(query)
 
         # similarity scores
-        similarity_scores = MODEL.similarity(query_embedding, corpus_embeddings)[0]
+        similarity_scores = MODEL.similarity(query_embedding,
+                                             corpus_embeddings)[0]
         top_k = min(top_k_min, len(self.df))
         scores, indices = torch.topk(similarity_scores, k=top_k)
 
@@ -160,29 +172,51 @@ class CorpusManager:
             row = self.df.iloc[int(idx)]
             row['score'] = float(score)
             rows.append(row)
+        logger.info(f'Returned {len(rows)} rows')
+        return rows
 
-        # dataframe
+    def similarity_by_paragraphs(
+            self,
+            query: str,
+            top_k_min: int=100
+            ) -> pd.DataFrame:
+        """
+        Retrieve similar rows, convert to df and sort by descending similarity.
+        """
+        rows = self._similarity_search(query=query, top_k_min=top_k_min)
         df = pd.DataFrame(rows).reset_index(drop=True)
         df = df.sort_values(by='score', ascending=False)
-        logger.info(f'returned query results with shape {df.shape}')
+        logger.info(f'Returned {len(df)} paragraphs')
         return df
 
-    def get_top_pages(df_sim: pd.DataFrame, top_n: int=20) -> list[str]:
-        "group similarity df by page name and calculate paragraph similarity average"
-        # todo: deduplicate page names
-        df_sim = df_sim.drop(columns=['paragraphs'])
-        df_sim_grouped = df_sim.groupby('page_name', as_index=False)['score'].mean()
-        df_sim_grouped = df_sim_grouped.sort_values(by='score', ascending=False)
-        df_sim_grouped = df_sim_grouped[:top_n]
-        top_pages = df_sim_grouped['page_name'].to_list()
-        logger.info(f'returned {len(top_pages)} top_pages')
-        return top_pages
+    def similarity_by_pages(
+            self,
+            query: str,
+            top_k_min: int=100
+            ) -> pd.DataFrame:
+        "Group by page name and calculate paragraph similarity average."
+        df = self.similarity_by_paragraphs(query=query, top_k_min=top_k_min)
+        dfg = df.groupby('page_name', as_index=False)['score'].mean()
+        dfg = dfg.sort_values(by='score', ascending=False)
+        logger.info(f'Returned {len(dfg)} pages')
+        return dfg
 
-    def get_page_group_dict(df):
-        """community_detection"""
-        df = df.groupby('page_name')['paragraphs'].apply(lambda paras: ' '.join(paras)).reset_index()
-        corpus_embedding = MODEL.encode_document(df['paragraphs'].tolist())
-        groups_lists = community_detection(corpus_embedding, min_community_size=10, threshold=.6)
+    def cluster_by_pages(
+            self,
+            min_community_size=20,
+            threshold=0.5
+            ) -> pd.DataFrame:
+        """Cluster the corpus by pages."""
+        #todo: pre-save the page corpus embeddings
+        df = self.df.groupby('page_name')['text'].apply(
+            lambda paras: ' '.join(paras)).reset_index()
+        paras_embedding = MODEL.encode_document(df['text'].tolist())
+
+        groups_lists = community_detection(
+            paras_embedding,
+            min_community_size=min_community_size,
+            threshold=threshold
+            )
 
         group_dfs = []
         for group_n, group in enumerate(groups_lists):
@@ -195,5 +229,4 @@ class CorpusManager:
             group_dfs.append(group_df)
 
         dfc = pd.concat(group_dfs)
-        group_dict = {k: v for k, v in zip(dfc['page_name'], dfc['group'])}
-        return group_dict
+        return dfc
