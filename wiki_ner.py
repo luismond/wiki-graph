898e0ece2a3f073eb74f84362c4106cc12624c51
luismond <luismond.nlp@gmail.com>
2025-12-22 02:27:55 -0600

    add page link builder

--- wiki_ner.py
+++ wiki_ner.py
@@ -3,16 +3,23 @@
 from __init__ import logger
 from crawler import Crawler
 from corpus_manager import CorpusManager
-
+from relationship_builder import RelationshipBuilder
+from network_visualizer import draw_graph_pyvis
 
 def main():
     logger.info('Starting main...')
-    for _ in range(5):
+    for _ in range(2):
         try:
-            crawler = Crawler()
+            crawler = Crawler(max_pages=10, max_new_pages=10)
             crawler.crawl()
             cm = CorpusManager()
             cm.load()
+            rlb = RelationshipBuilder()
+            rlb.build_page_links()
+            dfr = rlb.read_page_links()
+            dfx = rlb.filter(dfr)
+            draw_graph_pyvis(dfx)
+
         except Exception as e:
             logger.warning(str(e))
     logger.info('Finished main')
