3c232e505988888a1226c8d1c10cf5be5703eef6
luismond <luismond.nlp@gmail.com>
2025-12-22 23:55:45 -0600

    Add argument parsing to main function for configurable runs and page limits

--- wiki_ner.py
+++ wiki_ner.py
@@ -1,5 +1,6 @@
 """Main wikiNER script."""
 
+import argparse
 from __init__ import logger
 from db_util import get_db_info
 from crawler import Crawler
@@ -8,11 +9,27 @@ from pages_graph import PagesGraph
 
 
 def main():
+    """
+    Main function to build the corpus.
+
+    Usage:
+        python wiki_ner.py --runs 10 --max-pages 10 --max-new-pages 10
+    """
     logger.info('Starting main...')
-    for _ in range(2):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--runs", type=int, required=True, default=5)
+    ap.add_argument("--max-pages", type=int, required=True, default=5)
+    ap.add_argument("--max-new-pages", type=int, required=True, default=5)
+    args = ap.parse_args()
+    logger.info(f'Runs: {args.runs}, max_pages: {args.max_pages}, '
+                f'max_new_pages: {args.max_new_pages}')
+
+    for n in range(args.runs):
+        logger.info(f'Run {n}')
         try:
             get_db_info()
-            crawler = Crawler(max_pages=15, max_new_pages=15)
+            crawler = Crawler(max_pages=args.max_pages,
+                              max_new_pages=args.max_new_pages)
             crawler.crawl()
             cm = CorpusManager()
             cm.load()
